48812924dfb73a542bb737ae1ac0823286ce3897
Brian Boughton <brian@brianboughton.com>
2014-12-03 15:36:32 -0700

    Using a single docker builder service
    We are consolidating all our docker building into a single docker server
    and changing the env variable.

--- bin/build.sh
+++ bin/build.sh
@@ -8,17 +8,17 @@ error_exit(){
 [ -n "${bamboo_DOCKERHUB_EMAIL}" ] || error_exit "bamboo_DOCKERHUB_EMAIL Required"
 [ -n "${bamboo_DOCKERHUB_USERNAME}" ] || error_exit "bamboo_DOCKERHUB_USERNAME Required"
 [ -n "${bamboo_DOCKERHUB_PASSWORD}" ] || error_exit "bamboo_DOCKERHUB_PASSWORD Required"
-[ -n "${bamboo_DOCKER_HOST}" ] || error_exit "bamboo_DOCKER_HOST required"
+[ -n "${bamboo_DOCKER_BUILDER}" ] || error_exit "bamboo_DOCKER_BUILDER required"
 [ -n "${IMAGE_NAME}" ] || error_exit "IMAGE_NAME required"
 
-docker --host ${bamboo_DOCKER_HOST} login \
+docker --host ${bamboo_DOCKER_BUILDER} login \
   --email ${bamboo_DOCKERHUB_EMAIL} \
   --username ${bamboo_DOCKERHUB_USERNAME} \
   --password ${bamboo_DOCKERHUB_PASSWORD} || \
   error_exit "Failed to login to DockerHub"
 
-docker --host ${bamboo_DOCKER_HOST} build --tag=${IMAGE_NAME} . || \
+docker --host ${bamboo_DOCKER_BUILDER} build --tag=${IMAGE_NAME} . || \
   error_exit "Failed to build image"
 
-docker --host ${bamboo_DOCKER_HOST} push ${IMAGE_NAME} || \
+docker --host ${bamboo_DOCKER_BUILDER} push ${IMAGE_NAME} || \
   error_exit "Unable to push image to DockerHub"
