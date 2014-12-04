330d350696c23adac59fe7863988d0304f1f0ce9
Brian Boughton <brian@brianboughton.com>
2014-12-03 22:14:16 -0700

    Secure connection to docker build
    Use tls to encrypt comunication to and from docker server

--- bin/build.sh
+++ bin/build.sh
@@ -11,14 +11,29 @@ error_exit(){
 [ -n "${bamboo_DOCKER_BUILDER}" ] || error_exit "bamboo_DOCKER_BUILDER required"
 [ -n "${IMAGE_NAME}" ] || error_exit "IMAGE_NAME required"
 
-docker --host ${bamboo_DOCKER_BUILDER} login \
+docker --tlsverify \
+  --tlscacert=/home/bamboo/.docker/ca.pem \
+  --tlscert=/home/bamboo/.docker/cert.pem \
+  --tlskey=/home/bamboo/.docker/key.pem \
+  --host ${bamboo_DOCKER_BUILDER} \
+  login \
   --email ${bamboo_DOCKERHUB_EMAIL} \
   --username ${bamboo_DOCKERHUB_USERNAME} \
   --password ${bamboo_DOCKERHUB_PASSWORD} || \
   error_exit "Failed to login to DockerHub"
 
-docker --host ${bamboo_DOCKER_BUILDER} build --tag=${IMAGE_NAME} . || \
+docker --tlsverify \
+  --tlscacert=/home/bamboo/.docker/ca.pem \
+  --tlscert=/home/bamboo/.docker/cert.pem \
+  --tlskey=/home/bamboo/.docker/key.pem \
+  --host ${bamboo_DOCKER_BUILDER} \
+  build --tag=${IMAGE_NAME} . || \
   error_exit "Failed to build image"
 
-docker --host ${bamboo_DOCKER_BUILDER} push ${IMAGE_NAME} || \
+docker --tlsverify \
+  --tlscacert=/home/bamboo/.docker/ca.pem \
+  --tlscert=/home/bamboo/.docker/cert.pem \
+  --tlskey=/home/bamboo/.docker/key.pem \
+  --host ${bamboo_DOCKER_BUILDER} \
+  push ${IMAGE_NAME} || \
   error_exit "Unable to push image to DockerHub"
